d28bee5353b8ac90f1a0321640ec75be2e97bb01
sil8293 <sil_8293@hotmail.com>
2015-11-10 15:38:41 +0100

    Terminada la pirámide
    ahora la pirámide se pinta en la mitad de la pantalla y en la posición
    correcta

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -15,13 +15,27 @@ public class Ejercicio1 extends GraphicsProgram{
 		int numLadrillosBase = 14;
 		int anchoLadrillo = 30;
 		int altoLadrillo=12;
-
+		
+		
+		
+		int altoPiramide=numLadrillosBase*altoLadrillo;
+		int basePiramide=numLadrillosBase*anchoLadrillo;
+		
+		
+		int alto=getHeight();
+		int ancho=getWidth();
+		
+		
+		//querio yo del futuro ya está terminada la pirámide
 
 		for(int j=0; j<numLadrillosBase ;j++){
-			for (int i=0; i< numLadrillosBase;i++){
+			for (int i=0; i< numLadrillosBase-j;i++){
 				GRect ladrillo= new GRect (anchoLadrillo,altoLadrillo);
-				add(ladrillo,anchoLadrillo*j/2 + anchoLadrillo*i,altoLadrillo*j-i);
-				pause(10);
+				
+				add(ladrillo,
+						ancho/2- basePiramide/2+ anchoLadrillo*j/2 + anchoLadrillo*i,
+						alto-2*altoLadrillo-altoLadrillo*j);
+				pause(40);
 			}
 			
 		}
